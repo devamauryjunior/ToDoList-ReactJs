8b02a3865944b3e6a9fc67ec74340aebfa061f4c
devamauryjunior <jobagpj@gmail.com>
2022-12-24 00:31:57 -0300

    feat: add componentes TarefasConcluidas

---
+++ src/components/TarefasConcluidas/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TarefasConcluidas(props) {
+    return (
+        <div className="TarefasConcluidas">
+            
+        </div>
+    )
+}
+
+export default TarefasConcluidas;
